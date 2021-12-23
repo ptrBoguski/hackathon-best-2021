3dcaed1e42f0d194a65cedccceabb19c78453faa
ablaszkiewicz <kqmdjc8@gmail.com>
2021-12-23 17:28:37 +0100

    Add file loading

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Path = System.IO.Path;
 
 namespace HackathonBEST
 {
@@ -20,9 +24,39 @@ namespace HackathonBEST
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("kernel32.dll")]
+        private static extern bool AttachConsole(int dwProcessId);
+        
         public MainWindow()
         {
             InitializeComponent();
+            AttachConsole(-1);
+        }
+
+        private void ImageDropZone_OnDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var filePath = ((string[]) e.Data.GetData(DataFormats.FileDrop))[0];
+                LoadFile(filePath);
+            }
+        }
+
+        private void BrowseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+
+            if (result ?? false)
+            {
+                LoadFile(fileDialog.FileName);
+            }
+        }
+
+        private void LoadFile(string path)
+        {
+            var name = Path.GetFileName(path);
+            FileNameLabel.Content = name;
         }
     }
 }
